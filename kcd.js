a26154202ff1840b01f76656bca9ee5528d396c5
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-01-19 03:54:44 +0900

    1/19 friday

---
+++ kcd.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button')
+button.addEventListener('click', function (){
+    var move = document.getElementById('h1')
+    var table2 = document.getElementById('table2')
+
+    table2.appendChild(move);
+});
+
+
+
+
